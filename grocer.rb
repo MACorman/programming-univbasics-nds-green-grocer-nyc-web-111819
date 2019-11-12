3cd6b94041c392db26a2c53116e4af02e988d610
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 02:52:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,12 @@ def mk_coupon_hash(c)
   }
 end
 
-
+def apply_coupon_to_cart(matching_item, coupon, cart)
+  matching_item[:count] -= coupon[:num]
+  item_with_coupon = mk_coupon_hash(coupon)
+  item_with_coupon[:clearance] = matching_item[:clearance]
+  cart << item_with_coupon
+end
 
 
 def apply_coupons(cart, coupons)
